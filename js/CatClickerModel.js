e5fdb9b75c4b76fc85e545627c42a2606b5b1234
Aori Nevo <aori.nevo@bloomingdales.com>
2016-11-04 16:05:23 -0400

    Broadcase counterChanged event so that cat level can update.

---
+++ js/CatClickerModel.js
@@ -0,0 +1,39 @@
+define([],
+function(){
+  var model = {
+    catList: [
+      {
+        name: "Cat1",
+        src: "https://lh3.ggpht.com/nlI91wYNCrjjNy5f-S3CmVehIBM4cprx-JFWOztLk7vFlhYuFR6YnxcT446AvxYg4Ab7M1Fy0twaOCWYcUk=s0#w=640&h=426",
+        numberOfClicks: 0,
+        level: "infant"
+      },
+      {
+        name: "Cat2",
+        src: "https://lh3.ggpht.com/kixazxoJ2ufl3ACj2I85Xsy-Rfog97BM75ZiLaX02KgeYramAEqlEHqPC3rKqdQj4C1VFnXXryadFs1J9A=s0#w=640&h=496",
+        numberOfClicks: 0,
+        level: "infant"
+      },
+      {
+        name: "Cat3",
+        src: "https://lh5.ggpht.com/LfjkdmOKkGLvCt-VuRlWGjAjXqTBrPjRsokTNKBtCh8IFPRetGaXIpTQGE2e7ZCUaG2azKNkz38KkbM_emA=s0#w=640&h=454",
+        numberOfClicks: 0,
+        level: "infant"
+      },
+      {
+        name: "Cat4",
+        src: "https://d1wn0q81ehzw6k.cloudfront.net/additional/thul/media/4e34feee0acdc38a?w=400&h=400",
+        numberOfClicks: 0,
+        level: "infant"
+      },
+      {
+        name: "Cat5",
+        src: "https://pbs.twimg.com/profile_images/1245113943/Larry_the_cat2.jpg",
+        numberOfClicks: 0,
+        level: "infant"
+      }
+    ]
+  }; 
+  
+  return model;
+});
